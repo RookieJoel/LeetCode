a1e771e7a9a863b9fda0fcc6eb451bffed441b5a
JoelShelby <jojackchanah@gmail.com>
2025-03-29 10:34:15 +0700

    0803-CHEAPEST-FLIGHTS-WITHIN-K-STOPS Stats: Time: 0 ms (100%), Space: 17.8 MB (72.07%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& f : flights) {
-            int u = f[0], v = f[1], w = f[2];
-            adj[u].push_back({v, w});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto& f : flights){
+            adj[f[0]].push_back({f[1], f[2]});
         }
 
-        // {cost, current city, stops used}
-        queue<tuple<int, int, int>> q;
-        vector<int> dist(n, INT_MAX);
-        q.push({0, src, 0});
+        vector<int> dist(n, 1e9);
         dist[src] = 0;
 
-        while (!q.empty()) {
-            auto [cost, city, stops] = q.front(); q.pop();
-
-            if (stops > k) continue;
-
-            for (auto [nei, price] : adj[city]) {
-                if (cost + price < dist[nei]) {
-                    dist[nei] = cost + price;
-                    q.push({dist[nei], nei, stops + 1});
+        for(int i = 0; i <= k; ++i){
+            vector<int> temp = dist;
+            for(int u = 0; u < n; ++u){
+                if(dist[u] == 1e9) continue;
+                for(auto [v, w] : adj[u]){
+                    if(temp[v] > dist[u] + w){
+                        temp[v] = dist[u] + w;
+                    }
                 }
             }
+            dist = temp;
         }
 
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
