58dbf4327b9337e58f1fcc5e1be92670e10945ea
JoelShelby <jojackchanah@gmail.com>
2025-03-30 10:16:24 +0700

    1706-MIN-COST-TO-CONNECT-ALL-POINTS Stats: Time: 114 ms (74.07%), Space: 106.3 MB (47.36%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -2,34 +2,37 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<int> minDist(n, INT_MAX);
-        vector<bool> visited(n, false);
-        set<pair<int, int>> pq; // {cost, node}
-
-        minDist[0] = 0;
-        pq.insert({0, 0});
-        int totalCost = 0;
-
-        while (!pq.empty()) {
-            auto [cost, u] = *pq.begin();
-            pq.erase(pq.begin());
-
-            if (visited[u]) continue;
-            visited[u] = true;
-            totalCost += cost;
-
-            for (int v = 0; v < n; ++v) {
-                if (!visited[v]) {
-                    int dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
-                    if (dist < minDist[v]) {
-                        pq.erase({minDist[v], v}); 
-                        minDist[v] = dist;
-                        pq.insert({dist, v});
-                    }
+        vector<vector<pair<int,int>>> adj(n); //adj[points[i]] = {points[j],weight}
+        vector<int> dist(n,1e9);
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int x = points[i][0];
+                int y = points[i][1];
+                int a = points[j][0];
+                int b = points[j][1];
+                int val = abs(x-a)+abs(y-b);
+                adj[i].push_back({j,val});
+                adj[j].push_back({i,val});
+            }
+        }
+        dist[0] = 0;
+        vector<int> visited(n);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
+        pq.push({0,0});
+        int total = 0;
+        while(!pq.empty()){
+            auto [weight,u] = pq.top();pq.pop();
+           if(visited[u])continue;
+           visited[u] = 1;
+           total+=weight; 
+            for(auto [v,w_uv] : adj[u]){
+                if(!visited[v] && dist[v] > w_uv){
+                        dist[v] = w_uv;
+                        pq.push({dist[v],v});
                 }
             }
         }
 
-        return totalCost;
+        return total;
     }
 };
