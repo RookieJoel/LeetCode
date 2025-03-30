c4180ad53c191b5288f71a2086c65bb92ea3c4ea
JoelShelby <jojackchanah@gmail.com>
2025-03-30 10:33:44 +0700

    0744-NETWORK-DELAY-TIME Stats: Time: 90 ms (44.17%), Space: 44.4 MB (61.79%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        //time[u][v] = weight
-        vector<int> visited(n+1),dist(n+1,INT_MAX);
         vector<vector<pair<int,int>>> adj(n+1);
         for(int i=0;i<times.size();i++){
-                int u = times[i][0];
-                int v = times[i][1];
-                int w = times[i][2];
-                adj[u].push_back({v,w});
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+            adj[u].push_back({v,w});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
-        pq.push({0,k});
+        vector<int> dist(n+1,1e9);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> q;
         dist[k] = 0;
-        int ans = -1;
-        while(!pq.empty()){
-            auto [w,u] = pq.top();pq.pop();
-            if(visited[u]) continue;
-            visited[u] = 1;
-            for(auto [v,wv] : adj[u]){
-                if(!visited[v]){
-                    if(dist[v] > dist[u]+wv){
-                        dist[v] = dist[u]+wv;
-                        pq.push({dist[v],v});
-                    }
+        q.push({0,k});
+        while(!q.empty()){
+            auto [w,u] = q.top();q.pop();
+            for(auto [v,w_uv] : adj[u]){
+                if(dist[v] > dist[u]+w_uv){
+                    dist[v] = dist[u]+w_uv;
+                    q.push({dist[v],v});
                 }
             }
         }
-        
-        for (int i = 1; i <= n; ++i) {
-            if (dist[i] == INT_MAX) return -1;
-            ans = max(ans, dist[i]);
+        int ans = -2;
+        for(int i=1;i<=n;i++){
+            if(dist[i] == 1e9) return -1;
+            ans = max(ans,dist[i]);
         }
-
         return ans;
     }
 };
