11601f37fd7653616ba2548e064f9abed475377d
JoelShelby <jojackchanah@gmail.com>
2025-04-22 19:43:56 +0700

    1706-MIN-COST-TO-CONNECT-ALL-POINTS Stats: Time: 56 ms (88.47%), Space: 18.5 MB (85.3%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -3,36 +3,26 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
         vector<vector<pair<int,int>>> adj(n); //adj[points[i]] = {points[j],weight}
-        vector<int> dist(n,1e9);
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int x = points[i][0];
-                int y = points[i][1];
-                int a = points[j][0];
-                int b = points[j][1];
-                int val = abs(x-a)+abs(y-b);
-                adj[i].push_back({j,val});
-                adj[j].push_back({i,val});
-            }
-        }
+        vector<int> dist(n,INT_MAX),visited(n);
         dist[0] = 0;
-        vector<int> visited(n);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
         pq.push({0,0});
         int total = 0;
         while(!pq.empty()){
-            auto [weight,u] = pq.top();pq.pop();
-           if(visited[u])continue;
-           visited[u] = 1;
-           total+=weight; 
-            for(auto [v,w_uv] : adj[u]){
-                if(!visited[v] && dist[v] > w_uv){
-                        dist[v] = w_uv;
-                        pq.push({dist[v],v});
+            auto [w,pi] = pq.top();pq.pop();
+            if(visited[pi]) continue;
+            visited[pi] = 1;
+            total += w;
+            for(int i=0;i<n;i++){
+                if(i == pi)continue;
+                int dif = abs(points[pi][0] - points[i][0]) + abs(points[pi][1] - points[i][1]);
+                if(dist[i] > dif){
+                    dist[i] = dif;
+                    pq.push({dif,i});
                 }
             }
         }
-
         return total;
+        
     }
 };
